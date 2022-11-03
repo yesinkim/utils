0c66c5eeb9f7981016f7907dac91cae24e234167
yesinkim <bailando.ys@gmail.com>
2022-11-03 13:24:48 +0900

    Add decorator.py

---
+++ decorator.py
@@ -0,0 +1,29 @@
+def logging(func):
+    """print spent time and return value"""
+    def clocked(*args):
+        start_time = time.perf_counter()
+        returned = func(*args)
+        end_time = time.perf_counter() - start_time
+        name = func.__name__
+        arg_str = ', '.join(repr(arg) for arg in args)
+        # print('[%0.8fs] %s(%s) -> %r' % (end_time, name, arg_str, returned))
+        print("===============logging_func===============")
+        print(f"[{time.perf_counter() - start_time:0.8f}s] {name}({arg_str}) -> {returned}")
+        return returned
+    return clocked
+
+def show_details(func):
+    """print function information"""
+    def clocked(*args):
+        start_time = time.perf_counter()
+        returned = func(*args)
+        end_time = time.perf_counter() - start_time
+        name = func.__name__
+        arg_str = ', '.join(repr(arg) for arg in args)
+        print("==================function info.==================")
+        print(f"function name         : {name}")
+        print(f"function arguments    : {arg_str}")
+        print(f"function spending time: {end_time:0.8f}s")
+        print(f"function return       : \n{returned}")
+        return returned
+    return clocked
